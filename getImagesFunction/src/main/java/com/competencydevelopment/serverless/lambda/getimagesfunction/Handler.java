40ae138da541654370f8706ec7df16d62ddc0dd3
Pavel Markovsky <markovskypavel@MBP-Pavel.lan>
2023-08-06 21:50:04 +0300

    Add save images function

--- getImagesFunction/src/main/java/com/competencydevelopment/serverless/lambda/getimagesfunction/Handler.java
+++ getImagesFunction/src/main/java/com/competencydevelopment/serverless/lambda/getimagesfunction/Handler.java
@@ -6,89 +6,91 @@ import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
 import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent;
 import com.amazonaws.xray.AWSXRay;
 import com.amazonaws.xray.AWSXRayRecorderBuilder;
+import com.amazonaws.xray.entities.Subsegment;
 import com.amazonaws.xray.strategy.sampling.AllSamplingStrategy;
-
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import software.amazon.awssdk.utils.ImmutableMap;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Optional;
 
-public class Handler implements RequestHandler<APIGatewayProxyRequestEvent, APIGatewayProxyResponseEvent>{
-  private static final Logger logger = LoggerFactory.getLogger(Handler.class);
-  private static final HashMap<String, String> headers = new HashMap<>();
-  private static final Gson gson = new GsonBuilder().setPrettyPrinting().create();
+public class Handler implements RequestHandler<APIGatewayProxyRequestEvent, APIGatewayProxyResponseEvent> {
 
-  static{
-    headers.put("Content-Type", "application/json");
-    headers.put("Access-Control-Allow-Origin", "*");
+    private static final Logger LOGGER = LoggerFactory.getLogger(Handler.class);
+    private static final Gson GSON = new GsonBuilder().setPrettyPrinting().create();
+    private static final Map<String, String> HEADERS = new HashMap<>();
 
-    AWSXRayRecorderBuilder builder = AWSXRayRecorderBuilder.standard();
-    builder.withSamplingStrategy(new AllSamplingStrategy());
-    AWSXRay.setGlobalRecorder(builder.build());
-  }
+    static {
+        HEADERS.put("Content-Type", "application/json");
+        HEADERS.put("Access-Control-Allow-Origin", "*");
+
+        AWSXRay.setGlobalRecorder(AWSXRayRecorderBuilder.standard()
+                .withSamplingStrategy(new AllSamplingStrategy())
+                .build());
+    }
+
+    @Override
+    public APIGatewayProxyResponseEvent handleRequest(APIGatewayProxyRequestEvent event, Context context) {
+        Subsegment subsegment = AWSXRay.beginSubsegment("Get Images");
+        APIGatewayProxyResponseEvent response;
+        try {
+            LOGGER.info("Environment: {}", GSON.toJson(System.getenv()));
+            LOGGER.info("Event: {}", GSON.toJson(event));
+
+            // var client = DynamoDbAsyncClient.builder()
+            //         .overrideConfiguration(ClientOverrideConfiguration.builder()
+            //                 .addExecutionInterceptor(new TracingInterceptor())
+            //                 .build())
+            //         .build();
+
+            var tableName = System.getenv("IMAGES_TABLE_NAME");
+//      var attributes = new HashMap<String, AttributeValue>();
+            var userName = getUserFromEvent(event);
+            subsegment.putAnnotation("UserId", userName);
+
+            LOGGER.info("body: {}", GSON.toJson(event.getBody()));
+            // var note = gson.fromJson(event.getBody(), Note.class);
+//      attributes.put("UserId", AttributeValue.builder().s(userName).build());
+//      attributes.put("NoteId", AttributeValue.builder().n(note.getNoteId()).build());
+//      attributes.put("Note", AttributeValue.builder().s(note.getNote()).build());
+
+            // var request = PutItemRequest.builder()
+            //         .tableName(tableName)
+            //        .item(attributes)
+            //         .build();
+
+            // client.putItem(request).join();
+            response = new APIGatewayProxyResponseEvent().withStatusCode(200)
+                    .withHeaders(HEADERS)
+                    .withIsBase64Encoded(false)
+                    .withBody("Hello from Lambda!");
+
+        } catch (Exception e) {
+            subsegment.addException(e);
+            throw e;
+        } finally {
+            AWSXRay.endSubsegment();
+        }
+        return response;
+    }
 
     private String getUserFromEvent(APIGatewayProxyRequestEvent event) {
         var userName = "unknown";
         var authorizer = event.getRequestContext().getAuthorizer();
-        if (authorizer == null){
-            logger.warn("null authorizer - cannot determine username");
+        if (authorizer == null) {
+            LOGGER.warn("null authorizer - cannot determine username");
             return userName;
         }
-        var claims = (Map<String, String>)authorizer.get("claims");
-        if (claims == null){
-            logger.warn("no JWT in authorizer - cannot determine username");
+        var claims = (Map<String, String>) authorizer.get("claims");
+        if (claims == null) {
+            LOGGER.warn("no JWT in authorizer - cannot determine username");
             return userName;
         }
         return claims.get("cognito:username");
     }
 
-  @Override
-  public APIGatewayProxyResponseEvent handleRequest(APIGatewayProxyRequestEvent event, Context context) {
-    var subsegment = AWSXRay.beginSubsegment("Get Images");
-    APIGatewayProxyResponseEvent response;
-    try {
-      logger.info("environment: {}", gson.toJson(System.getenv()));
-      logger.info("event: {}", gson.toJson(event));
-
-      // var client = DynamoDbAsyncClient.builder()
-      //         .overrideConfiguration(ClientOverrideConfiguration.builder()
-      //                 .addExecutionInterceptor(new TracingInterceptor())
-      //                 .build())
-      //         .build();
-              
-      var tableName = System.getenv("IMAGES_TABLE_NAME");
-//      var attributes = new HashMap<String, AttributeValue>();
-      var userName = getUserFromEvent(event);
-      subsegment.putAnnotation("UserId", userName);
-
-      logger.info("body: {}", gson.toJson(event.getBody()));
-      // var note = gson.fromJson(event.getBody(), Note.class);
-//      attributes.put("UserId", AttributeValue.builder().s(userName).build());
-//      attributes.put("NoteId", AttributeValue.builder().n(note.getNoteId()).build());
-//      attributes.put("Note", AttributeValue.builder().s(note.getNote()).build());
-
-      // var request = PutItemRequest.builder()
-      //         .tableName(tableName)
-      //        .item(attributes)
-      //         .build();
-
-      // client.putItem(request).join();
-      response = new APIGatewayProxyResponseEvent()
-              .withStatusCode(200)
-              .withHeaders(headers)
-              .withIsBase64Encoded(false)
-              .withBody("Hello from Lambda!");
-              
-    }catch (Exception e) {
-      subsegment.addException(e);
-      throw e;
-    }finally {
-      AWSXRay.endSubsegment();
-    }
-    return response;
-  }
 }
--- saveImagesFunction/src/main/java/com/competencydevelopment/serverless/lambda/saveimagesfunction/Handler.java
+++ saveImagesFunction/src/main/java/com/competencydevelopment/serverless/lambda/saveimagesfunction/Handler.java
@@ -6,89 +6,100 @@ import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
 import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent;
 import com.amazonaws.xray.AWSXRay;
 import com.amazonaws.xray.AWSXRayRecorderBuilder;
+import com.amazonaws.xray.entities.Segment;
+import com.amazonaws.xray.entities.Subsegment;
 import com.amazonaws.xray.strategy.sampling.AllSamplingStrategy;
-
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
-
+import org.apache.commons.codec.binary.Base64;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import software.amazon.awssdk.core.sync.RequestBody;
+import software.amazon.awssdk.enhanced.dynamodb.DynamoDbEnhancedClient;
+import software.amazon.awssdk.enhanced.dynamodb.TableSchema;
+import software.amazon.awssdk.services.dynamodb.DynamoDbClient;
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.GetUrlRequest;
+import software.amazon.awssdk.services.s3.model.PutObjectRequest;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.UUID;
 
-public class Handler implements RequestHandler<APIGatewayProxyRequestEvent, APIGatewayProxyResponseEvent>{
-  private static final Logger logger = LoggerFactory.getLogger(Handler.class);
-  private static final HashMap<String, String> headers = new HashMap<>();
-  private static final Gson gson = new GsonBuilder().setPrettyPrinting().create();
+public class Handler implements RequestHandler<APIGatewayProxyRequestEvent, APIGatewayProxyResponseEvent> {
 
-  static{
-    headers.put("Content-Type", "application/json");
-    headers.put("Access-Control-Allow-Origin", "*");
+    private static final String TABLE_NAME = System.getenv("IMAGES_TABLE_NAME");
+    private static final String S3_BUCKET_NAME = System.getenv("S3_BUCKET_NAME");
 
-    AWSXRayRecorderBuilder builder = AWSXRayRecorderBuilder.standard();
-    builder.withSamplingStrategy(new AllSamplingStrategy());
-    AWSXRay.setGlobalRecorder(builder.build());
-  }
+    private static final Logger LOGGER = LoggerFactory.getLogger(Handler.class);
+    private static final Gson GSON = new GsonBuilder().setPrettyPrinting().create();
+    private static final Map<String, String> HEADERS = new HashMap<>();
 
-    private String getUserFromEvent(APIGatewayProxyRequestEvent event) {
-        var userName = "unknown";
-        var authorizer = event.getRequestContext().getAuthorizer();
-        if (authorizer == null){
-            logger.warn("null authorizer - cannot determine username");
-            return userName;
-        }
-        var claims = (Map<String, String>)authorizer.get("claims");
-        if (claims == null){
-            logger.warn("no JWT in authorizer - cannot determine username");
-            return userName;
-        }
-        return claims.get("cognito:username");
+    static {
+        HEADERS.put("Content-Type", "application/json");
+        HEADERS.put("Access-Control-Allow-Origin", "*");
+
+        AWSXRay.setGlobalRecorder(AWSXRayRecorderBuilder.standard()
+                .withSamplingStrategy(new AllSamplingStrategy())
+                .build());
     }
 
-  @Override
-  public APIGatewayProxyResponseEvent handleRequest(APIGatewayProxyRequestEvent event, Context context) {
-    var subsegment = AWSXRay.beginSubsegment("Get Images");
-    APIGatewayProxyResponseEvent response;
-    try {
-      logger.info("environment: {}", gson.toJson(System.getenv()));
-      logger.info("event: {}", gson.toJson(event));
+    @Override
+    public APIGatewayProxyResponseEvent handleRequest(APIGatewayProxyRequestEvent event, Context context) {
+        Subsegment subsegment = AWSXRay.beginSubsegment("Save Image");
+        APIGatewayProxyResponseEvent response;
+        try {
+            String imageName = UUID.randomUUID().toString();
+            String imageUrl = saveBytesToS3(imageName, event.getBody().getBytes());
 
-      // var client = DynamoDbAsyncClient.builder()
-      //         .overrideConfiguration(ClientOverrideConfiguration.builder()
-      //                 .addExecutionInterceptor(new TracingInterceptor())
-      //                 .build())
-      //         .build();
-              
-      var tableName = System.getenv("IMAGES_TABLE_NAME");
-//      var attributes = new HashMap<String, AttributeValue>();
-      var userName = getUserFromEvent(event);
-      subsegment.putAnnotation("UserId", userName);
+            LOGGER.info("Image uploaded to S3");
 
-      logger.info("body: {}", gson.toJson(event.getBody()));
-      // var note = gson.fromJson(event.getBody(), Note.class);
-//      attributes.put("UserId", AttributeValue.builder().s(userName).build());
-//      attributes.put("NoteId", AttributeValue.builder().n(note.getNoteId()).build());
-//      attributes.put("Note", AttributeValue.builder().s(note.getNote()).build());
+            Image image = new Image(imageName, imageUrl, null);
+            saveToDynamoDb(image);
 
-      // var request = PutItemRequest.builder()
-      //         .tableName(tableName)
-      //        .item(attributes)
-      //         .build();
+            LOGGER.info("Image uploaded to DynamoDB: {}", imageName);
 
-      // client.putItem(request).join();
-      response = new APIGatewayProxyResponseEvent()
-              .withStatusCode(200)
-              .withHeaders(headers)
-              .withIsBase64Encoded(false)
-              .withBody("Hello from Lambda!");
-              
-    }catch (Exception e) {
-      subsegment.addException(e);
-      throw e;
-    }finally {
-      AWSXRay.endSubsegment();
+            subsegment.putAnnotation("Image uploaded: ", GSON.toJson(image));
+
+            response = new APIGatewayProxyResponseEvent().withStatusCode(200)
+                    .withHeaders(HEADERS)
+                    .withIsBase64Encoded(false)
+                    .withBody(GSON.toJson(image));
+        } catch (Exception e) {
+            subsegment.addException(e);
+            throw e;
+        } finally {
+            AWSXRay.endSubsegment();
+        }
+        return response;
     }
-    return response;
-  }
+
+    private String saveBytesToS3(String key, byte[] fileBytes) {
+        try (S3Client s3Client = S3Client.builder().build()) {
+            PutObjectRequest putObjectRequest = PutObjectRequest.builder().bucket(S3_BUCKET_NAME).key(key).build();
+            s3Client.putObject(putObjectRequest, RequestBody.fromBytes(Base64.decodeBase64(fileBytes)));
+
+            GetUrlRequest getUrlRequest = GetUrlRequest.builder().bucket(S3_BUCKET_NAME).key(key).build();
+            return s3Client.utilities().getUrl(getUrlRequest).toString();
+        }
+    }
+
+    private void saveToDynamoDb(Image image) {
+        DynamoDbEnhancedClient.builder()
+                .dynamoDbClient(DynamoDbClient.builder().build())
+                .build()
+                .table(TABLE_NAME, TableSchema.fromBean(Image.class))
+                .putItem(image);
+    }
+
+    public static void main(String[] args) {
+        Segment mySegment = AWSXRay.beginSegment("do-startup-operation");
+        DynamoDbEnhancedClient.builder()
+                .dynamoDbClient(DynamoDbClient.builder().build())
+                .build()
+                .table("images", TableSchema.fromBean(Image.class))
+                .putItem(new Image("aa1", null, null));
+        AWSXRay.endSegment();
+    }
+
 }
